453d50d4bd1720493a0acc41c24a7d56eda9e50f
Eugene Choe <eugene.choe@gmail.com>
2017-04-06 20:15:36 -0700

    Add redux modules

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/src'));
 
+app.get('*', (req, res) => {
+  return res.sendFile(__dirname + '/src/index.html');
+});
+
 console.log('listening on port 1337');
 app.listen(1337);
